b58c3157d3bd50ee6386edef1c53a0f3332b223c
KatoTimothy <40914849+KatoTimothy@users.noreply.github.com>
2020-07-07 20:36:55 +0300

    Corrected syntax of the columns of the tables

--- app/src/main/java/android/app/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/android/app/notekeeper/NoteKeeperDatabaseContract.java
@@ -20,9 +20,9 @@ public final class NoteKeeperDatabaseContract {
         //CREATE TABLE course_info(_id, course_id,course_title)
         public static final String SQL_CREATE_TABLE=
                 "CREATE TABLE "+ TABLE_COURSE_INFO + "(" +
-                        _ID + "INTEGER PRIMARY KEY,"+
-                        COLUMN_COURSE_ID + "TEXT UNIQUE NOT NULL,"+
-                        COLUMN_COURSE_TITLE + "TEXT NOT NULL)";
+                        _ID + " INTEGER PRIMARY KEY,"+
+                        COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL,"+
+                        COLUMN_COURSE_TITLE + " TEXT NOT NULL)";
     }
 
     //The NoteInfoEntry table
@@ -40,7 +40,7 @@ public final class NoteKeeperDatabaseContract {
         //CREATE TABLE note_info (_id, note_title, note_text, course_id)
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE "+ TABLE_NOTE_INFO + "("+
-                        _ID + "INTEGER PRIMARY KEY, "+
+                        _ID + " INTEGER PRIMARY KEY, "+
                         COLUMN_NOTE_TITLE + " TEXT NOT NULL, "+
                         COLUMN_NOTE_TEXT + " TEXT,"+
                         COLUMN_COURSE_ID +" TEXT NOT NULL)";
